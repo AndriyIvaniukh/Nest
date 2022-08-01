9566c84b69676e337978532266a7dfe710d1ee5e
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-08-01 16:19:26 +0300

    v1.0.0

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {CreateUserDto} from "./dto/create-user.dto";
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {
+    }
+
+
+    @Get()
+    getAllUsers() {
+        return this.userService.getAll();
+    }
+
+    @Get('/:id')
+    getUserById(@Param() id: string){
+        return this.userService.getById(id);
+    }
+
+    @Post()
+    createUser(@Body() user: CreateUserDto){
+        return this.userService.create(user);
+    }
+
+    @Patch()
+    updateUser(){
+
+    }
+
+    @Delete()
+    deleteUser(){
+
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import {CreateUserDto} from "./dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private users = [];
+
+    getAll(){
+        return this.users;
+    }
+
+    getById(id: string){
+        return this.users.find((user)=> user.id == id);
+    }
+
+    create(user: CreateUserDto){
+        this.users.push({
+            ...user,
+            id: new Date().valueOf()
+        });
+        return user;
+    }
+
+    update(updateUserFields){
+        let userToUpdate = this.users.find((user) => user.id = updateUserFields.id);
+        userToUpdate = {...userToUpdate, ...updateUserFields};
+    }
+
+    delete(id: string){
+
+    }
+}
